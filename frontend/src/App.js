6b8e38b861c1ee591957baf757dde453508c0e71
Sumair Qaisar <sumairqaisar992@gmail.com>
2023-01-14 20:41:31 +0500

    Add google analytics

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
+import ReactGA from "react-ga";
 
 import { About, Footer, Header, Skills, Testimonial, Work } from "./container";
 import { Navbar } from "./components";
 import "./App.scss";
 
 const App = () => {
+	useEffect(() => {
+		ReactGA.initialize("G-KDKNWQGP8W");
+		ReactGA.pageview(window.location.pathname + window.location.search);
+	}, []);
 	return (
 		<div className='app'>
 			<Navbar />
