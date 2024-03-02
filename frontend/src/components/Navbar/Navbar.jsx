96f467b018b646136646dd27fc71be3ebad5f127
sumairq <sumairqaisar992@gmail.com>
2024-03-02 18:05:18 +0500

    Implement new header design

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
 	return (
 		<nav className='app__navbar'>
 			<div className='app__navbar-logo'>
-				<h1>Sumair</h1>
+				<h1>S<span>Q</span></h1>
 			</div>
 			<ul className='app__navbar-links'>
 				{["home", "about", "work", "skills", "contact"].map((item) => (
--- frontend/src/container/Header/Header.jsx
+++ frontend/src/container/Header/Header.jsx
@@ -1,29 +1,41 @@
-import React from "react";
-import { motion } from "framer-motion";
+import React from 'react';
+import { motion } from 'framer-motion';
 
-import AppWrap from "../../wrapper/AppWrap";
+import AppWrap from '../../wrapper/AppWrap';
 
-import "./Header.scss";
+import './Header.scss';
 
 const Header = () => {
-	return (
-		<div className='app__header app__flex'>
-			<div h-text>
-				<motion.h1
-					initial={{ opacity: 0, textDecoration: "underline" }}
-					whileInView={{ opacity: 1, textDecoration: "none" }}
-					className='greet-text'
-				>
-					Hi There ! <span>I'm Sumair</span>
-				</motion.h1>
-				<h1 className='title-text'>Front End Developer</h1>
-				<h1 className='mission-text'>
-					I design and develop expereinces that make people's lives{" "}
-					<span className='bold'>simpler</span>.
-				</h1>
+  return (
+    <div className="app__header">
+      <div h-text>
+        <motion.h1
+          initial={{ opacity: 0, textDecoration: 'underline' }}
+          whileInView={{ opacity: 1, textDecoration: 'none' }}
+          className="greet-text"
+        >
+		   <h3 className='small-name'><span>Sumair</span> Qaisar</h3>
+		   <div className='head-container'>
+
+			<div className='main-head'>
+          <span>Designer</span>
+          <span>Problem Solver</span>
+          <span>Collaborator</span>
+          <span className='dev-span'>Developer</span>
 			</div>
-		</div>
-	);
+			<div className='head-image'>
+				<img src='avatar.jpg' alt='avatar'></img>
+			</div>
+		   </div>
+        </motion.h1>
+        {/* <h1 className="title-text">Front End Developer</h1>
+        <h1 className="mission-text">
+          I design and develop expereinces that make people's lives{' '}
+          <span className="bold">simpler</span>.
+        </h1> */}
+      </div>
+    </div>
+  );
 };
 
-export default AppWrap(Header, "home");
+export default AppWrap(Header, 'home');
